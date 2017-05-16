ec1591c6a8b1f59893448a5660a22526147c6bb7
ying <wyingtian@gmail.com>
2017-05-16 13:51:38 -0400

    poll every 500ms for price data

--- airbnb_web_crawl/spiders/AirbnbSpider.py
+++ airbnb_web_crawl/spiders/AirbnbSpider.py
@@ -3,19 +3,28 @@ import json
 import time
 from selenium import webdriver
 from airbnb_web_crawl.items import AirbnbWebCrawlItem
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class AirbnbSpider(scrapy.Spider):
 
-    Query = "allston/homes?checkin=2017-05-18&checkout=2017-05-20"
+    #Query = "allston/homes?adults=1&checkin=2017-05-17&checkout=2017-05-18&ne_lat=42.36447710622584&ne_lng=-71.1279552729207&search_by_map=true&sw_lat=42.35651342299473&sw_lng=-71.13625939203081&zoom=16&s_tag=Xzaipvc6"
+
+    neighborhood = "allston/homes?"
+    checkin = "&checkin=2017-05-17"
+    checkout = "&checkout=2017-05-18"
+    location_on_map = "&ne_lat=42.36447710622584&ne_lng=-71.1279552729207&search_by_map=true&sw_lat=42.35651342299473&sw_lng=-71.13625939203081&zoom=16&s_tag=Xzaipvc6"
+
     name = "airbnb_spider"
 
-    start_urls = ['http://www.airbnb.com/s/' + Query]
+    start_urls = ['http://www.airbnb.com/s/' + neighborhood + checkin + checkout + location_on_map]
 
 
     def __init__(self):
         self.driver = webdriver.Chrome()
 
     def parse(self, response):
-        last_page_number = 40
+        last_page_number = 4
         if last_page_number < 1:
             return
         else:
@@ -36,9 +45,17 @@ class AirbnbSpider(scrapy.Spider):
         # get total price, including cleaning fee and service fee
         # use tr[last() in xpath to get total data
         self.driver.get(response.url)
-        time.sleep(3)
-        price = self.driver.find_element_by_xpath(
-               '//*[@id="summary"]/div/div/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div/form/div/div/div[2]/div/table/tbody/tr[last()]/td/span/span')
+
+        try:
+            price = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="summary"]/div/div/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div/form/div/div/div[2]/div/table/tbody/tr[last()]/td/span/span'))
+            )
+        finally:
+            print("no no no")
+            # self.driver.quit()
+        #
+        #price = self.driver.find_element_by_xpath(
+        #        '//*[@id="summary"]/div/div/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div/form/div/div/div[2]/div/table/tbody/tr[last()]/td/span/span')
         if price:
             item['price'] = price.text
 
@@ -47,6 +64,5 @@ class AirbnbSpider(scrapy.Spider):
         desc = response.xpath('//meta[@property="og:title"]/@content').extract()[0].split('-')[0]
         if desc:
             item['description'] = desc
-
 
         yield item
